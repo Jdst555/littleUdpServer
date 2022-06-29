b03018f2a3e65a477ad9cf739881cf3e7681d60b
Jdst555 <jasaad@hotmail.com>
2022-06-28 20:08:38 -0500

    client added here

--- src/main/java/udpserver/EchoServer.java
+++ src/main/java/udpserver/EchoServer.java
@@ -32,6 +32,7 @@ public class EchoServer extends Thread{
 	            
 	            InetAddress address = packet.getAddress();
 	            int port = packet.getPort();
+	            
 	            packet = new DatagramPacket(buf, buf.length, address, port);
 	            String received 
 	              = new String(packet.getData(), 0, packet.getLength());
--- src/main/java/udpserver/RunServer.java
+++ src/main/java/udpserver/RunServer.java
@@ -2,16 +2,23 @@ package udpserver;
 
 import java.net.SocketException;
 
+import client.EchoClient;
+
 public class RunServer {
 
+	
+	static EchoServer es;
+	static EchoClient ec = new EchoClient();
 	public static void main(String[] args) {
 		try {
-			EchoServer es = new EchoServer();
-			es.run();
+			es = new EchoServer();
+			es.start();
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		String reply = ec.sendEcho("end");
+		System.out.println(reply);
 		
 
 	}
