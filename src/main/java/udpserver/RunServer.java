6f8545ad97c366be622a301584e58495a05b91ac
Jdst555 <jasaad@hotmail.com>
2022-06-28 20:34:26 -0500

    Client deleted. Only Server

--- src/main/java/udpserver/RunServer.java
+++ src/main/java/udpserver/RunServer.java
@@ -2,13 +2,13 @@ package udpserver;
 
 import java.net.SocketException;
 
-import client.EchoClient;
+
 
 public class RunServer {
 
 	
 	static EchoServer es;
-	static EchoClient ec = new EchoClient();
+	
 	public static void main(String[] args) {
 		try {
 			es = new EchoServer();
@@ -17,8 +17,7 @@ public class RunServer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String reply = ec.sendEcho("end");
-		System.out.println(reply);
+		
 		
 
 	}
